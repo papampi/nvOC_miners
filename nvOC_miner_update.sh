77f0e5ed748f32d37b3e50af7124c1aab6a8002e
Payam Nab <papampi@users.noreply.github.com>
2018-12-15 10:52:08 +0330

    t-rex 0.8.8 cuda9.2

--- nvOC_miner_update.sh
+++ nvOC_miner_update.sh
@@ -107,8 +107,8 @@ TPccminer_ver_9="2.3"
 TPccminer_compiled_tarball_ver_9="TPccminer-2.3.tar.xz"
 TPccminer_src_hash_ver_9="370684f7435d1256cbabef4410a57ed5bc705fdc"
 
-T_Rex_ver="0.8.3"
-T_Rex_compiled_tarball="t-rex-0.8.3-linux-cuda9.2.tar.xz"
+T_Rex_ver="0.8.8"
+T_Rex_compiled_tarball="t-rex-0.8.8-linux-cuda9.2.tar.xz"
 
 VERTMINER_ver_8="1.0.2"
 VERTMINER_compiled_tarball_ver_8="vertminer-nvidia-1.0-stable.2.tar.xz"
