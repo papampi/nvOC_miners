e9fdc185bc4c4fcdcb076f5d880a60a1a237b7ae
Payam Nab <papampi@users.noreply.github.com>
2019-03-12 03:25:00 +0330

    Create xmr-stak.nvoc.sh

---
+++ XMR_Stak/2.10.1/xmr-stak.nvoc.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+my_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Remove nvidia.txt if cards count changed
+if [[ -f "$my_path/nvidia.txt" ]]
+then
+    if [ "$(grep -c "// gpu:" $my_path/nvidia.txt)" != "$(nvidia-smi -i 0 --query-gpu=count --format=csv,noheader,nounits)" ]
+    then
+        rm "$my_path/nvidia.txt"
+    fi
+fi
+# Change verbosity to show hashrate in output log
+if [[ -f "$my_path/config.txt" ]]
+then
+    if grep -q '"verbose_level" : 3' "$my_path/config.txt"
+    then
+        sed -i 's/"verbose_level" : 3/"verbose_level" : 4/g' "$my_path/config.txt"
+    fi
+fi
+
+# Large memory page support
+sudo sysctl -w vm.nr_hugepages=128
+
+"$my_path/xmr-stak" $@
